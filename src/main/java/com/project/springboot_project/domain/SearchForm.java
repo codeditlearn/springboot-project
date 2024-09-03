5ccb727ae1929abacba3eb822c48b9998570c195
codeditlearn <149052182+codeditlearn@users.noreply.github.com>
2024-09-03 16:01:53 +0900

    Delete src directory

--- src/main/java/com/project/springboot_project/domain/SearchForm.java
+++
@@ -1,8 +0,0 @@
-package com.project.springboot_project.domain;
-
-import lombok.Data;
-
-@Data
-public class SearchForm {
-    private String search;
-}
--- src/main/java/com/project/springboot_project/domain/dto/ValidMemberDto.java
+++
@@ -1,9 +0,0 @@
-package com.project.springboot_project.domain.dto;
-
-import lombok.Data;
-
-@Data
-public class ValidMemberDto {
-    private String userId;
-    private String userPw;
-}
